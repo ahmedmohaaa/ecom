3b04413290bb029671428fe12a6af6a63b25a3dc
ahmedmohaaa <ma2012924@gmail.com>
2025-08-03 21:05:34 +0300

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -126,3 +126,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
